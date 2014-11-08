45d722c5b39e6d107d556466bad24b356827aa4b
dumbastic <dumbastic@gmail.com>
2014-11-08 13:53:56 +0000

    Update settings

--- AdBeacon/settings.py
+++ AdBeacon/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'publish',
     'rest_framework',
+    'corsheaders',
+    'gunicorn'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -62,11 +64,6 @@ WSGI_APPLICATION = 'AdBeacon.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {}
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(default='postgres://codeepy:hackneee@adbeacon.cwwkieja1mig.eu-west-1.rds.amazonaws.com:5432/publish')
