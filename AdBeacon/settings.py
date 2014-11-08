c5b17a4c85a592f4d1b2fb0d4414cb3789034ba7
Abdu <abduhussein@gmail.com>
2014-11-08 13:16:26 +0000

    Model and Webservice test DB check

--- AdBeacon/settings.py
+++ AdBeacon/settings.py
@@ -61,12 +61,12 @@ WSGI_APPLICATION = 'AdBeacon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(default='postgres://codeepy:hackneee@adbeacon.cwwkieja1mig.eu-west-1.rds.amazonaws.com:5432/publish')
