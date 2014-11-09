4e804635581547582c76cd774b31bd5a7abe24fa
dumbastic <dumbastic@gmail.com>
2014-11-09 00:21:55 +0000

    Add registration and login

--- publish/urls.py
+++ publish/urls.py
@@ -6,5 +6,5 @@ from publish import views
 urlpatterns = [
     url(r'^$', views.index, name='register'),
     url(r'^account/(?P<id>\d)', views.account, name='account'),
-    url(r'^advertisement/', views.makeAdvs, name='imageupload'),
+    url(r'^advertisement/', views.advertisement, name='imageupload'),
 ]
--- publish/views.py
+++ publish/views.py
@@ -1,4 +1,7 @@
 import braintree
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
 
 braintree.Configuration.configure(braintree.Environment.Sandbox,
                                   merchant_id="4nvngnk2nqdy9pvk",
@@ -6,7 +9,7 @@ braintree.Configuration.configure(braintree.Environment.Sandbox,
                                   private_key="ce7fe085f1c98cced6e187122eea64b0")
 
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from publish.serializers import AdvtSerializer, DevSerializer
@@ -18,51 +21,6 @@ from django.core.urlresolvers import reverse
 
 def index(request):
     return render(request, "register.html")
-
-def login(request):
-    return render(request, "login.html")
-
-def register(request):
-    return render(request, "register.html")
-
-def account(request, id):
-    return render(request, "account.html")
-
-def payment(request):
-    return render(request, "payment.html", {"client_token": braintree.ClientToken.generate(), "msg": "lala"})
-
-def checkout(request):
-    client_token = braintree.ClientToken.generate()
-    return client_token
-
-def create_purchase(request):
-    if request.method == 'POST':
-        nonce = request.POST["payment_method_nonce"]
-        result = braintree.Transaction.sale({
-            "amount": "10.00",
-            "payment_method_nonce": nonce,
-            "options": {
-                "submit_for_settlement": True
-            }
-        })
-
-        if result.is_success:
-            transaction = result.transaction
-            if transaction.status == "processor_declined":
-                return render(request, "result.html", {"status": transaction.status, "detail": result.transaction.processor_response_text})
-            elif transaction.status == "settlement_declined":
-                return render(request, "result.html", {"status": transaction.status, "detail": result.transaction.processor_settlement_response_text})
-            elif transaction.status == "gateway_rejected":
-                return render(request, "result.html", {"status": transaction.status, "detail": result.transaction.gateway_rejection_reason})
-
-            return render(request, "result.html", {"status": transaction.status, "detail": ""})
-        else:
-            return render(request, "result.html", {"status": result.errors.deep_errors, "detail": ""})
-
-
-def advertisement(request):
-    return render(request, "advertisement.html")
-
 
 class AdvtViewSet(viewsets.ModelViewSet):
     """
@@ -92,7 +50,8 @@ def adv_detail_by_loc_id(request, param):
         serializer = AdvtSerializer(ads, many=True)
         return JSONResponse(serializer.data)
 
-def makeAdvs(request):
+@login_required(login_url='/login/')
+def advertisement(request):
 
     if request.method=="POST":
         adv = AdvForm(request.POST, request.FILES)
@@ -108,3 +67,79 @@ def makeAdvs(request):
         images = Advertisement.objects.all()
         beacons = BeaconDevice.objects.all()
         return render(request, 'advertisement.html', {'form': adv, 'images': images, 'beacons': beacons})
+
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+    return render_to_response('login.html', context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def register_user(request):
+    from django.contrib.auth.models import User
+
+    if request.POST:
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            user = User.objects.create_user(username=username, email=email, password=password1)
+            user.save()
+            print 'Registration successful'
+            return HttpResponseRedirect('/')
+        else:
+            print 'Password is not matched!'
+
+    return render(request, "register.html")
+
+@login_required(login_url='/login/')
+def account(request, id):
+    return render(request, "account.html")
+
+@login_required(login_url='/login/')
+def payment(request):
+    return render(request, "payment.html", {"client_token": braintree.ClientToken.generate()})
+
+@login_required(login_url='/login/')
+def checkout(request):
+    client_token = braintree.ClientToken.generate()
+    return client_token
+
+@login_required(login_url='/login/')
+def create_purchase(request):
+    if request.method == 'POST':
+        nonce = request.POST["payment_method_nonce"]
+        result = braintree.Transaction.sale({
+            "amount": "10.00",
+            "payment_method_nonce": nonce,
+            "options": {
+                "submit_for_settlement": True
+            }
+        })
+
+        if result.is_success:
+            transaction = result.transaction
+            if transaction.status == "processor_declined":
+                return render(request, "result.html", {"status": transaction.status, "detail": result.transaction.processor_response_text})
+            elif transaction.status == "settlement_declined":
+                return render(request, "result.html", {"status": transaction.status, "detail": result.transaction.processor_settlement_response_text})
+            elif transaction.status == "gateway_rejected":
+                return render(request, "result.html", {"status": transaction.status, "detail": result.transaction.gateway_rejection_reason})
+
+            return render(request, "result.html", {"status": transaction.status, "detail": ""})
+        else:
+            return render(request, "result.html", {"status": result.errors.deep_errors, "detail": ""})
+
